8bedf566d47e33a5f44eef2dcf4357992948c0ec
giadavolpin <volpingiada@gmail.com>
2022-11-08 17:43:22 +0100

    modifiche

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ const app = createApp({
         console.log();
     },
     created(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
-            console.log(result.data.response);
-            this.mails.push(result.data.response);
-        })
+       
         for(let i = 0; i<10; i++ ){
-            this.mails();
+            this.mails;
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
+                console.log(result.data.response);
+                this.mails.push(result.data.response);
+            })
         }
        
     }
